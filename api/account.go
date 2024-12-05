e8315a8a476ae599016fbb2cd346c4500ab6dd6f
LMafra <chagas.lucas.mafra@gmail.com>
2024-12-05 17:26:15 -0300

    add more routes

--- api/account.go
+++ api/account.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	db "github.com/lmafra/simplebank/db/sqlc"
 	"net/http"
@@ -106,4 +108,34 @@ func (server *Server) updateAccount(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, account)
+}
+
+type deleteAccountRequest struct {
+	ID int64 `uri:"id" binding:"required",min=1`
+}
+
+func (server *Server) deleteAccount(ctx *gin.Context) {
+	var req deleteAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	if req.ID <= 0 {
+		ctx.JSON(http.StatusBadRequest, errorResponse(fmt.Errorf("invalid account ID")))
+		return
+	}
+
+	err := server.store.DeleteAccount(ctx, req.ID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			ctx.JSON(http.StatusNotFound, errorResponse(fmt.Errorf("account not found")))
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": "account successfully deleted"})
 }
